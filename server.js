233bc9d6e94d765f8a7a9d31d9b969e9ed21e2cd
srivastavayushi <srivastavayushi@users.noreply.github.com>
2021-01-20 00:39:02 +0530

    added routes

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API RUNNING');
+// Connect Database
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API RUNNING");
 });
+
+// Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/profile", require("./routes/profile"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/posts", require("./routes/posts"));
 
 const PORT = process.env.PORT || 5000;
 
