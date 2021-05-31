2c8f68212b2ba9d67bf0e2adf88205d1f49fe7dd
srivastavayushi <srivastavayushi@users.noreply.github.com>
2021-05-31 19:38:53 +0530

    feat : verify JWT Token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 // @route   GET api/auth
 // @desc    TEST route
 // @access  Public
 
-router.get("/", (req, res) => {
+router.get("/", auth, (req, res) => {
   res.send("Auth Route");
 });
 
