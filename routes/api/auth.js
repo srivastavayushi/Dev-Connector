dc32efe66203e4501aa6695e97192351a0b0bedf
srivastavayushi <srivastavayushi@users.noreply.github.com>
2021-05-31 19:56:29 +0530

    feat : auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,12 +2,20 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 
+const User = require("../../model/User");
+
 // @route   GET api/auth
 // @desc    TEST route
 // @access  Public
 
-router.get("/", auth, (req, res) => {
-  res.send("Auth Route");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
